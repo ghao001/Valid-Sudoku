4ebad99f21a27313264ac524d7e40fdd49bafb45
ghao001 <36051327+ghao001@users.noreply.github.com>
2018-07-08 01:37:38 -0700

    Update Solition.java

--- Solition.java
+++ Solition.java
@@ -4,21 +4,32 @@ class Solution {
         for(int i=0;i<9;i++)
             for(int j=0;j<9;j++)
             {
+                int k=0;
                 if(board[i][j]!='.')
                 {
                     if(map.containsKey(board[i][j]))
                     {
-                        int k=0;
+                      
                         while(map.get(board[i][j])[k][0]!=-1)
                         {
                             if(map.get(board[i][j])[k][0]==i||map.get(board[i][j])[k][1]==j)
                                 return false;
-                            else if()
+                            else if(map.get(board[i][j])[k][0]>=i-i%3&&map.get(board[i][j])[k][0]<i-i%3+3)
+                                    if(map.get(board[i][j])[k][1]>=j-j%3&&map.get(board[i][j])[k][1]<j-j%3+3)
+                                        return false;
+                            k++;
                         }
+                        
                     }
+                    else{
+                        map.add(board[i][j],new int[8]);
+                    }
+                    map.get(board[i][j])[k][0]=i;
+                        map.get(board[i][j])[k][1]=j;
+                        map.get(board[i][j])[k+1][0]=-1;
                 }
             }
                 
-
+    return true;
     }
 }
